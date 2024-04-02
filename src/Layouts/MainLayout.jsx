653c81eabac335035b7be737dadcdc26c7f6338e
pantho0 <panthomashreky6@gmail.com>
2024-04-02 21:45:55 +0600

    added basic appbar for navigation

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/shared/Navbar";
 
 
 const MainLayout = () => {
     return (
         <div>
+            <Navbar/>
             this is main layout
             <Outlet/>
         </div>
