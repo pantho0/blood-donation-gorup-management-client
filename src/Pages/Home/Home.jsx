500a80ed1150fd761b1da4c0dc2b11d898e2d032
pantho0 <panthomashreky6@gmail.com>
2024-04-03 21:40:17 +0600

    bannder section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,18 +1,26 @@
-import { Container, Grid } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
+import bannerImage from "../../assets/images/banner/pexels-photo-12193105.jpeg";
 
 const Home = () => {
   return (
-    <Container maxWidth='lg'>
-<Grid container spacing={2}>
-      <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-            hello
+    <Container maxWidth="lg">
+      <Grid container spacing={0}>
+        <Grid item xs={12} sm={12} md={6} lg={6} xl={6} sx={{display:'flex', flexDirection:"column", justifyContent:'center'}}>
+         <Box sx={{display:'flex', justifyContent:'start', alignItems:'center', marginBottom:'10px'}}>
+            <Typography variant="h2" fontWeight={700} fontSize={40} color={'primary'}>
+                Donate Blood, Save Life
+            </Typography>
+         </Box>
+         <Box>
+         <Typography maxWidth={'80%'} textAlign={'justify'}>
+         Join us in the noble cause of saving lives. Your blood donation can be the lifeline for someone in need. Together, let's make a difference in the world by giving the gift of life. Every drop counts, every donor matters. Donate blood and be a hero in someone's story.
+         </Typography>
+         </Box>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+          <Box component="img" alt="banner" src={bannerImage} />
+        </Grid>
       </Grid>
-      <Grid item  xs={12} sm={12} md={6} lg={6} xl={6}>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-  <path fill="#880e4f" fillOpacity="1" d="M0,32L120,58.7C240,85,480,139,720,176C960,213,1200,235,1320,245.3L1440,256L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"></path>
-</svg>
-      </Grid>
-    </Grid>
     </Container>
   );
 };
